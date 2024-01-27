d3fb88ec7d494e0a9e24b7778395b86184596821
Eastlake FTC <eastlakeftc@gmail.com>
2024-01-27 15:14:36 -0800

    took out importing CRServo since we aren't using those anymore.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newDrive.java
@@ -4,7 +4,6 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name = "New Drive")
